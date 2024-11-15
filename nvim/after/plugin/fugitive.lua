45dc48db958393240b18de9cb50ad390a7145ef5
Sven Skender <sskender@pm.me>
2024-11-15 19:46:44 +0100

    Add missing descriptions to keymaps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true, desc = "Show Git status" })
+vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { noremap = true, silent = true, desc = "Show Git log" })
--- nvim/lua/sskender/mappings.lua
+++ nvim/lua/sskender/mappings.lua
@@ -1,8 +1,11 @@
 -- window navigation
-vim.keymap.set("n", "<C-j>", "<C-W>j", { silent = true })
-vim.keymap.set("n", "<C-k>", "<C-W>k", { silent = true })
-vim.keymap.set("n", "<C-h>", "<C-W>h", { silent = true })
-vim.keymap.set("n", "<C-l>", "<C-W>l", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-W>j", { silent = true, desc = "Move window down" })
+vim.keymap.set("n", "<C-k>", "<C-W>k", { silent = true, desc = "Move window up" })
+vim.keymap.set("n", "<C-h>", "<C-W>h", { silent = true, desc = "Move window left" })
+vim.keymap.set("n", "<C-l>", "<C-W>l", { silent = true, desc = "Move window right" })
+
+-- buffer quick jump
+vim.keymap.set("n", "<leader>l", ":b#<CR>", { silent = true, desc = "Jump to last buffer" })
 
 -- jump to first non-blank character
 vim.keymap.set("n", "0", "^")
