f43d561bd10340e1a004aac1dc5ad8cd562802e2
Sven Skender <sskender@pm.me>
2024-11-15 19:44:30 +0100

    Display relative file path in lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,6 +3,13 @@ local custom_gruvbox = require("lualine.themes.gruvbox")
 custom_gruvbox.normal.c.bg = "#112233"
 
 require("lualine").setup {
+    sections = {
+        lualine_c = {
+            {
+                "filename", path = 1
+            }
+        }
+    },
     options = {
         theme = custom_gruvbox,
         icons_enabled = false,
