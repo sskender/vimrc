f4889332ec7a9adea471cc0a96831b8de01a90d8
Sven Skender <sskender@pm.me>
2024-11-22 21:51:24 +0100

    Fix lualine separators

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -5,14 +5,14 @@ custom_gruvbox.normal.c.bg = "#112233"
 require("lualine").setup {
     sections = {
         lualine_c = {
-            {
-                "filename", path = 1
-            }
+            { "filename", path = 1 },
         }
     },
     options = {
         theme = custom_gruvbox,
         icons_enabled = false,
         global_status = true,
+        component_separators = "",
+        section_separators = "",
     }
 }
