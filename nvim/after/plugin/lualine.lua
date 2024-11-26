377f3f15b6b625635c6aa2ef203daba76623ec33
Sven Skender <sskender@pm.me>
2024-11-26 16:55:23 +0100

    Improve lualine sections and colors

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,12 +1,25 @@
 local custom_gruvbox = require("lualine.themes.gruvbox")
 
-custom_gruvbox.normal.c.bg = "#112233"
+custom_gruvbox.normal.c.bg = "NONE"
+custom_gruvbox.insert.c.bg = "NONE"
+custom_gruvbox.visual.c.bg = "NONE"
+custom_gruvbox.replace.c.bg = "NONE"
+custom_gruvbox.command.c.bg = "NONE"
+custom_gruvbox.inactive.c.bg = "NONE"
+
+custom_gruvbox.normal.c.fg = "#a89984"
+custom_gruvbox.insert.c.fg = "#a89984"
+custom_gruvbox.visual.c.fg = "#a89984"
+custom_gruvbox.replace.c.fg = "#a89984"
+custom_gruvbox.command.c.fg = "#a89984"
+custom_gruvbox.inactive.c.fg = "#a89984"
 
 require("lualine").setup {
     sections = {
-        lualine_c = {
-            { "filename", path = 1 },
-        }
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = {},
+        lualine_y = { "encoding", "fileformat", "filetype" },
+        lualine_z = { "location" }
     },
     options = {
         theme = custom_gruvbox,
